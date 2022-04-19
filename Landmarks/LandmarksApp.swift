4d683c81fbd6b9c043f5aa7b7f6ed404b64455e2
Casey Mockbee <caseymockbee97@gmail.com>
2022-04-19 14:47:26 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Casey Mockbee on 4/19/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
